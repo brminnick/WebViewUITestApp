0755b274abfa90f14065549d3fb2bfbc22c466cc
brminnick <brandon.r.minnick@gmail.com>
2017-06-17 22:10:46 -0700

    Updated NuGet Packages

--- WebViewUITestApp/WebViewUITestApp/App.cs
+++ WebViewUITestApp/WebViewUITestApp/App.cs
@@ -14,20 +14,5 @@ namespace WebViewUITestApp
 				}
 			};
 		}
-
-		protected override void OnStart()
-		{
-			// Handle when your app starts
-		}
-
-		protected override void OnSleep()
-		{
-			// Handle when your app sleeps
-		}
-
-		protected override void OnResume()
-		{
-			// Handle when your app resumes
-		}
 	}
 }
