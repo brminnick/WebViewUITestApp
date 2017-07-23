c630bb1cf872551c02111ae45177cdc019d6afbe
brminnick <brandon.r.minnick@gmail.com>
2017-07-23 15:23:43 -0700

    Moved PCL to NET Standard

--- WebViewUITestApp.iOS/AppDelegate.cs
+++ WebViewUITestApp.iOS/AppDelegate.cs
@@ -20,12 +20,12 @@ namespace WebViewUITestApp.iOS
         //
         // You have 17 seconds to return from this method, or iOS will terminate your application.
         //
-        public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+        public override bool FinishedLaunching(UIApplication uiApplication, NSDictionary launchOptions)
         {
             global::Xamarin.Forms.Forms.Init();
             LoadApplication(new App());
 
-            return base.FinishedLaunching(app, options);
+            return base.FinishedLaunching(uiApplication, launchOptions);
         }
     }
 }
